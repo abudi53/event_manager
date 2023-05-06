3bfccb720785426ab54b144c6c175bce6ca51b40
abudi53 <abdallahffh@gmail.com>
2023-05-05 23:13:17 -0400

    Finish personalized thank you letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,32 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+
+# def puts_content(content)
+#   contents.each do |row|
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+
+#     puts "#{name} #{zipcode} #{legislators}"
+#   end
+# end
+
+def content_sub(row, template_letter)
+  # Substitutes the content of the csv file into the template letter
+  # the row is the selected row from the csv file
+
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = template_letter.result(binding)
+
+  save_thank_you_letters(id, form_letter)
+end
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -14,7 +40,7 @@ def legislators_by_zipcode(zip)
     legislators = civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
     legislator_names = legislators.map(&:name)
     legislator_names.join(', ')
@@ -25,33 +51,22 @@ end
 
 puts 'EventManager initialized.'
 
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts(form_letter)
+  end
+end
+
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
 
-def puts_content(content)
-  contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zipcode(zipcode)
-
-    puts "#{name} #{zipcode} #{legislators}"
-  end
-end
-
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
-
-def content_sub(content, template_letter)
-  name = content[:first_name]
-  zipcode = clean_zipcode(content[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = template_letter.result(binding)
-  form_letter
-end
 
 contents.each do |row|
   puts content_sub(row, erb_template)
