0ebdbbf4ef27d47ed216f49cffd5c3cd72358232
abudi53 <abdallahffh@gmail.com>
2023-05-05 21:34:08 -0400

    Add erb template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -15,11 +15,10 @@ def legislators_by_zipcode(zip)
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
+    ).officials
     legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
-  rescue
+    legislator_names.join(', ')
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
@@ -32,12 +31,28 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents.each do |row|
-  name = row[:first_name]
+def puts_content(content)
+  contents.each do |row|
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
 
-  zipcode = clean_zipcode(row[:zipcode])
+    puts "#{name} #{zipcode} #{legislators}"
+  end
+end
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+def content_sub(content, template_letter)
+  name = content[:first_name]
+  zipcode = clean_zipcode(content[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = template_letter.result(binding)
+  form_letter
+end
+
+contents.each do |row|
+  puts content_sub(row, erb_template)
 end
